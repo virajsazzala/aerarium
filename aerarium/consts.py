4405838eec1cded9860d6ab1fb913447c26ee024
SNG Viraj Reddy <vir200319@gmail.com>
2023-11-28 18:06:33 +0530

    update config dirs
    Signed-off-by: SNG Viraj Reddy <vir200319@gmail.com>

--- aerarium/consts.py
+++ aerarium/consts.py
@@ -1,5 +1,4 @@
 import os
 
-# Only use this path during test. Change later to a more appropriate path.
-DATA_FILE_PATH = os.path.join(os.path.dirname(__file__), 'data.json')
-CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), 'config.yaml')
+DATA_FILE_PATH = "~/.config/aerarium/transactions.json"
+CONFIG_FILE_PATH = "~/.config/aerarium/config.json"
