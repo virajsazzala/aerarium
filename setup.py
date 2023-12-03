494bf5862d1fe4e3050ed72fe66af1d2fcfb76fc
SNG Viraj Reddy <vir200319@gmail.com>
2023-12-04 04:01:55 +0530

    add setup.py
    Signed-off-by: SNG Viraj Reddy <vir200319@gmail.com>

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open("README.rst", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="aerarium",
+    version="1.0.0",
+    author="SNG Viraj Reddy",
+    author_email="vir200319@gmail.com",
+    description="A CLI tool to track finances",
+    long_description=long_description,
+    license="MIT",
+    url="https://github.com/virajsazzala/aerarium",
+    packages=find_packages(),
+    install_requires=[
+        "PyYAML",
+    ],
+    scripts=["scripts/aerarium"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
